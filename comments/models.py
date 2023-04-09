14e99767bd95e126bdebd5143dd88edd555be434
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-04-09 22:56:44 +0200

    Fix showing posted date and time

--- comments/models.py
+++ comments/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
 
 class Comment(models.Model):
     content = models.CharField(max_length=50)
     date = models.DateTimeField()
+
     def __str__(self):
         return self.content
+
+    def is_today(self):
+        return self.date.date() == timezone.localdate()
