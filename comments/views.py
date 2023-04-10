ac63c32d1566b0a29a1196f46156167aa9c4dff5
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2023-04-10 12:56:19 +0200

    Show converted time to users time

--- comments/views.py
+++ comments/views.py
@@ -4,15 +4,23 @@ from django.utils import timezone
 
 from .models import Comment
 
+
 def index(request):
     comments = Comment.objects.all()
-    context = {"comments": comments}
+    timezone_name = request.COOKIES.get("timezone_name")
+    if timezone_name:
+        context = {"comments": comments, "user_timezone": str(timezone_name)}
+    else:
+        context = {"comments": comments}
+
     return render(request, "index.html", context)
+
 
 def add_comment(request):
     new_comment = Comment(content=request.POST["comment"], date=timezone.now())
     new_comment.save()
     return HttpResponseRedirect("/")
+
 
 def delete_comments(request):
     Comment.objects.all().delete()
